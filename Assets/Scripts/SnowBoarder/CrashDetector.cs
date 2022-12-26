8e0405bc5506d9861a29f45b8e5929ce080e4f01
Juan Pablo Garcia <drandx@users.noreply.github.com>
2022-12-25 23:58:12 -0800

    Renaming changes

--- Assets/Scripts/SnowBoarder/CrashDetector.cs
+++ Assets/Scripts/SnowBoarder/CrashDetector.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionDetector : MonoBehaviour
+public class CrashDetector : MonoBehaviour
 {
     // Detect when an object collides with the object
     private void OnTriggerEnter2D(Collider2D collision)
