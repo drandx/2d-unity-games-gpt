3e4c9d33916a8eacaef89ad9026d9031692dd2b4
Juan Pablo Garcia <drandx@users.noreply.github.com>
2022-12-26 00:48:09 -0800

    Changes

--- Assets/Scripts/SnowBoarder/CrashDetector.cs
+++ Assets/Scripts/SnowBoarder/CrashDetector.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
+    // Time to reload the scene
+    [SerializeField] float timeToReload = 0.5f;
+
     // Detect when an object collides with the object
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-        // If the object is the player
-        if (collision.gameObject.tag == "Ground")
+        if (other.gameObject.tag == "Ground")
         {
-            // Print object tag
-            Debug.Log("Collision");
+            Invoke("ReloadScene", timeToReload);
         }
+    }
+    // Method to reload the scene with a timeout
+    void ReloadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/SnowBoarder/FinishLine.cs
+++ Assets/Scripts/SnowBoarder/FinishLine.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float timeToReload = 0.5f;
+
     // Detect when Riders enters in the trigger
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // If the rider is the player
         if (collision.gameObject.tag == "Rider")
         {
-            // Print object tag
-            Debug.Log("Finish it");
+            Invoke("ReloadScene", timeToReload);
         }
+    }
+
+    // Method to reload the scene with a timeout
+    void ReloadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
